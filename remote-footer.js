82c435624e26cf25d39a8114dea621f2cc4e892f
afrorakda <gadws00@gmail.com>
2026-04-15 01:56:54 +0800

    Update remote-footer.js

--- remote-footer.js
+++ remote-footer.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+(function() {
     const footerElem = document.getElementById('shared-footer');
     if (footerElem) {
         footerElem.innerHTML = 'No login / No tracking / No ads / Browser only <a href="https://afrorakda.github.io/privacy-policy.html" style="color:var(--main); text-decoration:underline;">Privacy Policy</a> / afrorakda © 2026';
     }
-});
+})();
